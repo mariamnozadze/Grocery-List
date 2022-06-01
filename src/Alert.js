ba6e81d30266d68d098a9b2b7fcd24bb36b8d99b
mariamnozadze <mariamnozadze97@yahoo.com>
2022-06-01 18:41:52 +0400

    - Created functions to deal with editing and removing items  - Added storage to save items to the list  - Added useEffect to deal with actions

--- src/Alert.js
+++ src/Alert.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
-const Alert = () => {
-  return (
-    <div>Alert</div>
-  )
-}
+const Alert = ({ type, msg, removeAlert, list}) => {
 
-export default Alert
+  //Removes alert after 3 seconds
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert();
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [list]);
+
+  return <p className={`alert alert-${type}`}>{msg}</p>;
+};
+
+export default Alert;
--- src/App.js
+++ src/App.js
@@ -1,32 +1,93 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import List from "./List";
 import "./index.css";
 import Alert from "./Alert";
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return JSON.parse(localStorage.getItem("list"));
+  } else {
+    return [];
+  }
+};
+
 function App() {
   const [name, setName] = useState("");
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(!name) {
+    if (!name) {
       //display alert
-    }else if (name && isEditing){
+      showAlert(true, "danger", "please enter value");
+    } else if (name && isEditing) {
       //deal with edit
-    }else {
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName("");
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, "success", "value changed");
+    } else {
       //show alert
-      const newItem = {id: new Date().getTime().toString(), title: name};
+      showAlert(true, "success", "Item added to the list");
+      const newItem = { id: new Date().getTime().toString(), title: name };
       setList([...list, newItem]);
-      setName('');
+      setName("");
     }
   };
+
+  //Displays alert type and message after action
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
+  };
+
+  //Clears all of the list items
+  const clearList = () => {
+    showAlert(true, "danger", "Empty List");
+    setList([]);
+  };
+
+  //Removes specific item from the list
+  const removeItem = (id) => {
+    showAlert(true, "danger", "item removed");
+    setList(list.filter((item) => item.id !== id));
+  };
+
+  //Edit specific item
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title);
+  };
+
+  //everytime list changes, saves latest values
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
+
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert />}
+        {alert.show && (
+          <Alert
+            {...alert}
+            removeAlert={showAlert}
+            list={list}
+            editItem={editItem}
+          />
+        )}
         <h3>grocery list</h3>
         <div className="form-control">
           <input
@@ -42,8 +103,10 @@ function App() {
         </div>
       </form>
       <div className="grocery-container">
-        <List items={list}/>
-        <button className="clear-btn">clear items</button>
+        <List items={list} removeItem={removeItem} editItem={editItem} />
+        <button className="clear-btn" onClick={clearList}>
+          clear items
+        </button>
       </div>
     </section>
   );
